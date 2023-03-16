525842446654df2088655cbb6b19e711ae082df3
Dzaky Tamir <dzakytamir@student.ub.ac.id>
2023-03-16 19:29:17 +0700

    Added triple buffering strategy and created blank displayed screen

--- src/com/zainic/zainiship/Main.java
+++ src/com/zainic/zainiship/Main.java
@@ -1,10 +1,124 @@
 package com.zainic.zainiship;
 
-public class Main {
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
+import javax.swing.JFrame;
+
+public class Main extends Canvas implements Runnable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private static int width = 300;
+	private static int height = width / 16 * 9;
+	private static int scale = 3;
+	private static String title = "Zainiship";
+	
+	private Thread thread;
+	private JFrame frame;
+	private boolean running = false;
+	
+	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
+	
+	public Main() {
+		Dimension size = new Dimension(width*scale, height*scale);
+		this.setPreferredSize(size);
+		
+		frame = new JFrame();
+	}
+	
+	public synchronized void start() {
+		running = true;
+		thread = new Thread(this, "Display");
+		thread.start();
+	}
+	
+	public synchronized void stop() {
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void run() {
+		long lastTime = System.nanoTime();
+		long timer = System.currentTimeMillis();
+		final double ns = 1000000000.0 / 60.0;
+		double delta = 0;
+		int frames = 0;
+		int ticks = 0;
+		this.requestFocus();
+		while (running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while (delta > 1) {
+				tick();
+				ticks++;
+				delta--;
+			}
+			render();
+			frames++;
+			
+			if (System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				frame.setTitle(title + " | " + ticks + "ups, " + frames + "fps");
+				ticks = 0;
+				frames = 0;
+			}
+		}
+		stop();
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if (bs == null) {
+			this.createBufferStrategy(3); //triple buffering strategy
+			return;
+		}
+		
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = 0xff00ff;
+		}
+		
+		Graphics g = bs.getDrawGraphics(); //link between graphics and buffer
+		g.setColor(new Color(255, 99, 33));
+		g.fillRect(0, 0, this.getWidth(), this.getHeight());
+		g.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), null);
+		//g.setColor(Color.WHITE);
+		//g.setFont(new Font("Courier New", Font.PLAIN, 20));
+		//g.drawString("X : "+player.x+" Y : "+player.y, 100, 100);
+		//g.fillRect(Mouse.getX() - 5, Mouse.getY() - 5, 10, 10);
+		//g.fillRect(Mouse.getX() - 5, Mouse.getY() - 5, 10, 10);
+		//g.drawString("Button : " + Mouse.getB(), 10, 10);
+		g.dispose(); //remove the graphics after not used
+		bs.show(); //show the buffer that being calculated
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Main game = new Main();
+		game.frame.setResizable(false);
+		game.frame.setTitle(Main.title);
+		game.frame.add(game);
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null);
+		game.frame.setVisible(true);
+		
+		game.start();
 	}
 
 }
