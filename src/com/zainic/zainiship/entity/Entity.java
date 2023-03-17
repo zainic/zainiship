64173243bc9f09768403721f69dd32bb0ba806e3
Dzaky Tamir <dzakytamir@student.ub.ac.id>
2023-03-17 13:38:06 +0700

    Modified Entity class

--- src/com/zainic/zainiship/entity/Entity.java
+++ src/com/zainic/zainiship/entity/Entity.java
@@ -1,5 +1,46 @@
 package com.zainic.zainiship.entity;
 
-public class Entity {
+import com.zainic.zainiship.screen.Screen;
+
+public abstract class Entity {
+	
+	private int x, y;
+	private boolean removed = false;
+	
+	public void init() {
+		
+	}
+	
+	public int getX() {
+		return this.x;
+	}
+	
+	public int getY() {
+		return this.y;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void render(Screen screen) {
+		
+	}
+	
+	public void remove() {
+		removed = true;
+	}
+	
+	public boolean isRemoved() {
+		return removed;
+	}
 
 }
